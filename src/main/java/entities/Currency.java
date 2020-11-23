04cd88138eb4a2d350c4e07a7cbb712773a62fb1
Jonas <j.b-n@live.dk>
2020-11-23 14:21:46 +0100

    currencies added

--- src/main/java/entities/Currency.java
+++ src/main/java/entities/Currency.java
@@ -10,24 +10,134 @@ package entities;
  * @author Jonas
  */
 public class Currency {
-    
+
+    private Double ALL;
+    private Double AMD;
+    private Double AZN;
+    private Double BAM;
+    private Double BGN;
+    private Double BYN;
+    private Double CHF;
+    private Double CZK;
+    private Double DKK;
     private Double EUR;
     private Double GBP;
+    private Double GEL;
+    private Double HRK;
+    private Double HUF;
+    private Double ISK;
+    private Double MDL;
+    private Double MKD;
+    private Double NOK;
+    private Double PLN;
+    private Double RON;
+    private Double RSD;
+    private Double RUB;
+    private Double SEK;
+    private Double TRY;
+    private Double UAH;
 
-    public Double getGBP() {
-        return GBP;
-    }
-
-    public void setGBP(Double GBP) {
-        this.GBP = GBP;
-    }
-
-    public Currency(Double EUR, Double GBP) {
+    public Currency(Double ALL, Double AMD, Double AZN, Double BAM, Double BGN, Double BYN, Double CHF, Double CZK, Double DKK, Double EUR, Double GBP, Double GEL, Double HRK, Double HUF, Double ISK, Double MDL, Double MKD, Double NOK, Double PLN, Double RON, Double RSD, Double RUB, Double SEK, Double TRY, Double UAH) {
+        this.ALL = ALL;
+        this.AMD = AMD;
+        this.AZN = AZN;
+        this.BAM = BAM;
+        this.BGN = BGN;
+        this.BYN = BYN;
+        this.CHF = CHF;
+        this.CZK = CZK;
+        this.DKK = DKK;
         this.EUR = EUR;
         this.GBP = GBP;
+        this.GEL = GEL;
+        this.HRK = HRK;
+        this.HUF = HUF;
+        this.ISK = ISK;
+        this.MDL = MDL;
+        this.MKD = MKD;
+        this.NOK = NOK;
+        this.PLN = PLN;
+        this.RON = RON;
+        this.RSD = RSD;
+        this.RUB = RUB;
+        this.SEK = SEK;
+        this.TRY = TRY;
+        this.UAH = UAH;
+    }
+
+    public Currency() {
     }
     
-    public Currency() {
+    public Double getALL() {
+        return ALL;
+    }
+
+    public void setALL(Double ALL) {
+        this.ALL = ALL;
+    }
+
+    public Double getAMD() {
+        return AMD;
+    }
+
+    public void setAMD(Double AMD) {
+        this.AMD = AMD;
+    }
+
+    public Double getAZN() {
+        return AZN;
+    }
+
+    public void setAZN(Double AZN) {
+        this.AZN = AZN;
+    }
+
+    public Double getBAM() {
+        return BAM;
+    }
+
+    public void setBAM(Double BAM) {
+        this.BAM = BAM;
+    }
+
+    public Double getBGN() {
+        return BGN;
+    }
+
+    public void setBGN(Double BGN) {
+        this.BGN = BGN;
+    }
+
+    public Double getBYN() {
+        return BYN;
+    }
+
+    public void setBYN(Double BYN) {
+        this.BYN = BYN;
+    }
+
+    public Double getCHF() {
+        return CHF;
+    }
+
+    public void setCHF(Double CHF) {
+        this.CHF = CHF;
+    }
+
+    public Double getCZK() {
+        return CZK;
+    }
+
+    public void setCZK(Double CZK) {
+        this.CZK = CZK;
+    }
+
+    public Double getDKK() {
+        return DKK;
+    }
+
+    public void setDKK(Double DKK) {
+        this.DKK = DKK;
     }
 
     public Double getEUR() {
@@ -37,5 +147,127 @@ public class Currency {
     public void setEUR(Double EUR) {
         this.EUR = EUR;
     }
+
+    public Double getGBP() {
+        return GBP;
+    }
+
+    public void setGBP(Double GBP) {
+        this.GBP = GBP;
+    }
+
+    public Double getGEL() {
+        return GEL;
+    }
+
+    public void setGEL(Double GEL) {
+        this.GEL = GEL;
+    }
+
+    public Double getHRK() {
+        return HRK;
+    }
+
+    public void setHRK(Double HRK) {
+        this.HRK = HRK;
+    }
+
+    public Double getHUF() {
+        return HUF;
+    }
+
+    public void setHUF(Double HUF) {
+        this.HUF = HUF;
+    }
+
+    public Double getISK() {
+        return ISK;
+    }
+
+    public void setISK(Double ISK) {
+        this.ISK = ISK;
+    }
+
+    public Double getMDL() {
+        return MDL;
+    }
+
+    public void setMDL(Double MDL) {
+        this.MDL = MDL;
+    }
+
+    public Double getMKD() {
+        return MKD;
+    }
+
+    public void setMKD(Double MKD) {
+        this.MKD = MKD;
+    }
+
+    public Double getNOK() {
+        return NOK;
+    }
+
+    public void setNOK(Double NOK) {
+        this.NOK = NOK;
+    }
+
+    public Double getPLN() {
+        return PLN;
+    }
+
+    public void setPLN(Double PLN) {
+        this.PLN = PLN;
+    }
+
+    public Double getRON() {
+        return RON;
+    }
+
+    public void setRON(Double RON) {
+        this.RON = RON;
+    }
+
+    public Double getRSD() {
+        return RSD;
+    }
+
+    public void setRSD(Double RSD) {
+        this.RSD = RSD;
+    }
+
+    public Double getRUB() {
+        return RUB;
+    }
+
+    public void setRUB(Double RUB) {
+        this.RUB = RUB;
+    }
+
+    public Double getSEK() {
+        return SEK;
+    }
+
+    public void setSEK(Double SEK) {
+        this.SEK = SEK;
+    }
+
+    public Double getTRY() {
+        return TRY;
+    }
+
+    public void setTRY(Double TRY) {
+        this.TRY = TRY;
+    }
+
+    public Double getUAH() {
+        return UAH;
+    }
+
+    public void setUAH(Double UAH) {
+        this.UAH = UAH;
+    }
+
     
+
 }
