208aa6d7378254e33b79e16db8ae75ae6c0b7cdb
Anne-Maj Larsen <annemajlarsen@gmail.com>
2020-11-26 14:52:34 +0100

    tests

--- src/test/java/facades/UserFacadeTest.java
+++ src/test/java/facades/UserFacadeTest.java
@@ -1,12 +1,21 @@
 package facades;
 
 import dtos.UserDTO;
+import entities.Favourite;
 import entities.User;
+import errorhandling.AlreadyExistsException;
+import errorhandling.MissingInputException;
 import errorhandling.NotFoundException;
 import java.util.ArrayList;
 import java.util.List;
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
+import static org.hamcrest.CoreMatchers.hasItems;
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
+import org.hamcrest.Matchers;
+import static org.hamcrest.Matchers.hasItem;
+import static org.hamcrest.Matchers.hasItems;
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Test;
@@ -24,6 +33,7 @@ public class UserFacadeTest {
     private static EntityManagerFactory emf;
     private static UserFacade facade;
     private static User u1, u2, u3;
+    private static Favourite f1, f2, f3;
 
     public UserFacadeTest() {
 
@@ -37,6 +47,14 @@ public class UserFacadeTest {
         u1 = new User("user11", "test11");
         u2 = new User("user12", "test12");
         u3 = new User("user13", "test13");
+
+        f1 = new Favourite("Belgium");
+        f2 = new Favourite("France");
+        f3 = new Favourite("Sweden");
+        u1.addFavourite(f1);
+        u1.addFavourite(f3);
+        u2.addFavourite(f2);
+        u3.addFavourite(f3);
 
         EntityManager em = emf.createEntityManager();
 
@@ -87,10 +105,35 @@ public class UserFacadeTest {
     @Test
     public void testGetAllUsers() {
         int expectedCount = 3;
-
         int actual = facade.getAllUsers().size();
-
         assertEquals(expectedCount, actual);
+    }
+
+    @Test
+    public void testGetFavouritesByCount() {
+        int actual = facade.getFavorites(u1.getUserName()).size();
+        int exp = 2;
+        assertEquals(exp, actual);
+
+    }
+
+    @Test
+    public void testGetFavouritesByContent() {
+        List<Favourite> favList = new ArrayList<Favourite>();
+        favList = facade.getFavorites(u1.getUserName());
+
+        assertThat(favList, hasItem(
+                Matchers.<Favourite>hasProperty("countryName", is("Sweden"))));
+
+    }
+
+    @Test
+    public void testAddFavourite() throws MissingInputException, AlreadyExistsException {
+        facade.addFavourite("Poland", u2.getUserName());
+        int actual = facade.getFavorites(u2.getUserName()).size();
+        int exp = 2;
+        assertEquals(exp,actual);
+        
     }
 
 }
